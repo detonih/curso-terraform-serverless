be541fea6d705d4dbb6d95b342bb61ed959d81da
Henrique Detoni <detonihenrique@gmail.com>
2020-07-29 10:07:16 -0300

    add iam role

--- api/register/handler.js
+++ api/register/handler.js
@@ -1,11 +1,11 @@
 'use strict';
 
-module.exports.hello = async event => {
+module.exports.register = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: process.env.DYNAMODB_USERS,
         input: event,
       },
       null,
